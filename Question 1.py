6f3bd2ec01f57e3d3b4d3a37f6009c12ac02bd0c
Kaaybe <125987617+Kaaybe@users.noreply.github.com>
2025-10-25 23:06:17 +0300

    Delete Question 1.py

--- Question 1.py
+++
@@ -1,7 +0,0 @@
-# Declare variables with different data types
-name = "John"  # String
-age = 20       # Integer
-is_AI_student = True  # Boolean
-
-# Print formatted output using f-string
-print(f"Hello, my name is {name}. I am {age} years old and AI student status is {is_AI_student}.")
